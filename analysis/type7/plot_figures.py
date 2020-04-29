5a3ce32b82d8563deb432ea531481b13077478cb
Xiaohui0726 <joy.cxh0726@gmail.com>
2020-04-29 10:02:53 +0800

    updat vis

--- analysis/type7/plot_figures.py
+++ analysis/type7/plot_figures.py
@@ -34,31 +34,32 @@ for l in list_7:
     # att_pkl = os.path.join(path,"{}.pkl".format(l))
     attentions = pickle.load(open(att_pkl,'rb'))
     tokens = attentions['tokens']
-    q_idx_st, q_idx_end = 1, tokens.index('[SEP]')
+    q_idx_st, q_idx_end = 0, tokens.index('[SEP]')
     c_idx_st, c_idx_end = q_idx_end+1, len(tokens)-1
 
-    q_tokens = [elem for _ in range(16) for elem in tokens[q_idx_st:q_idx_end]]+['']
+    q_tokens = tokens[q_idx_st:q_idx_end]
     c_tokens = tokens[c_idx_st:c_idx_end]
-    # c_string = tokenizer.convert_tokens_to_string(c_tokens)
-    # c_string = '\''.join(c_string.split(' \' '))
+    c_string = tokenizer.convert_tokens_to_string(c_tokens)
+    c_string = '\''.join(c_string.split(' \' '))
 
     answer_tokens = [answer['text'] for answer in attentions['answers']]
 
     answer_ids = [tokenizer.encode(text)[1:-1] for text in answer_tokens]
     c_ids = tokenizer.convert_tokens_to_ids(c_tokens)
 
-    data = np.vstack([attentions['attention'][4][att_idx][c_idx_st:c_idx_end,q_idx_st:q_idx_end].T for att_idx in range(16)])
+    data = np.vstack([attentions['attention'][4][att_idx][0, 1:].T for att_idx in range(16)])
+    data = data[:, :len(tokens)+1]
 
     max_entry = np.max(data)
-    gt = np.zeros((1,data.shape[1]))
+    gt = np.zeros((1, data.shape[1]))
     indices = [find_sub_list(answer_id, c_ids) for answer_id in answer_ids]
 
     for indexxx in indices:
         for indexx in indexxx:
             for tkidx in range(indexx[0], indexx[-1]+1):
-                    gt[:, tkidx] = 0.5
+                    gt[:, q_idx_end+tkidx] = 0.5
 
     data = np.vstack([data,gt])
     plt.figure(figsize = (50,50))
-    ax = seaborn.heatmap(data, xticklabels= c_tokens, yticklabels= q_tokens, square=True, vmin=0.0, vmax=0.5,cbar=False)
+    ax = seaborn.heatmap(data, xticklabels= tokens, yticklabels= list(range(16)), square=True, vmin=0.0, vmax=0.5,cbar=False)
     ax.figure.savefig('./has_answer/'+l.split('/')[-1].replace('pkl','png'))
